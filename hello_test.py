856b640e14be529ae1a20d19309c532e3b9d61d5
YaronDavid <urlof_desty91@hotmail.com>
2023-06-29 15:16:55 -0600

    change test

--- hello_test.py
+++ hello_test.py
@@ -5,7 +5,7 @@ class TestHello(unittest.TestCase):
     def test_greet(self):
         name = "Jenkins"
         result = greet(name)
-        self.assertEqual(result, "Hello, Jenkins!")
+        self.assertEqual(result, "Hi, Jenkins!")
 
 if __name__ == "__main__":
     unittest.main()
